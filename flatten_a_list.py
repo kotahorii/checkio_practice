dd3d5e5f3843a43385f46e98dbbe00c009d967d3
kotahorii <khorii.micron@gmail.com>
2022-02-17 19:25:07 +0900

    [Update] solved question

--- flatten_a_list.py
+++ flatten_a_list.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def first_word(text: str) -> str:
     text_list = text.split(" ")
     return text_list[0]
@@ -11,5 +14,12 @@ def number_length(num: int) -> int:
     return len(str(num))
 
 
+def most_frequent(strings: list[str]) -> str:
+    count = Counter(strings)
+    for k, v in count.items():
+        if v >= len(strings) % 2 + len(strings) // 2:
+            return k
+
+
 if __name__ == "__main__":
-    print(number_length(10))
+    print(most_frequent(["a", "b", "c", "a", "b", "a"]))
