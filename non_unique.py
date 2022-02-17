451cb7c2a596265cbfd610a74409ba9cacffa059
kotahorii <khorii.micron@gmail.com>
2022-02-17 19:04:09 +0900

    [Update] solved question

--- non_unique.py
+++ non_unique.py
@@ -1,12 +1,20 @@
-def checkio(data: list[int]) -> list[int]:
-    i = 0
-    while i < len(data):
-        if data.count(data[i]) == 1:
-            data.pop(i)
-        else:
-            i += 1
+from collections import Counter
 
-    return data
+# def checkio(data: list[int]) -> list[int]:
+#     i = 0
+#     while i < len(data):
+#         if data.count(data[i]) == 1:
+#             data.pop(i)
+#         else:
+#             i += 1
+
+#     return data
+
+
+# Best solution
+def checkio(data):
+    count = Counter(data)
+    return [n for n in data if count[n] > 1]
 
 
 if __name__ == "__main__":
